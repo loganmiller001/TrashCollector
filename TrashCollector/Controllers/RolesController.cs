4ec95679e3dcb5b1e66a7a92aea4863dc47b9754
loganmiller001 <loganmiller001@live.com>
2018-07-26 10:50:52 -0500

    Created a rolescontroller class that can create and index the uuser roles.

---
+++ TrashCollector/Controllers/RolesController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TrashCollector.Models;
+
+namespace TrashCollector.Controllers
+{
+    public class RolesController : Controller
+    {
+        ApplicationDbContext context;
+
+        public void RoleController()
+        {
+            context = new ApplicationDbContext();
+            
+        }
+        // GET: Roles
+        public ActionResult Index()
+        {
+            var roles = context.Roles.ToList();
+            return View(roles);
+        }
+
+        public ActionResult Create()
+        {
+            var role = new IdentityRole();
+            return View(role);
+        }
+
+        [HttpPost]
+        public ActionResult Create(IdentityRole role)
+        {
+            context.Roles.Add(role);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
