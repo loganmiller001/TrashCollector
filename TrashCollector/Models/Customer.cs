033b76b086822bf6c09bcc7843043266fc2148f1
loganmiller001 <loganmiller001@live.com>
2018-07-26 10:29:35 -0500

    Added initial customer and employee models, gave them properties and set them to the local db.

---
+++ TrashCollector/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StreetAddress { get; set; }
+        public int Zipcode { get; set; }
+        public int AmmountDue { get; set; }
+    }
+}
