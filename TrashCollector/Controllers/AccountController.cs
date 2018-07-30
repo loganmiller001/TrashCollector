820b77403a850dd62c17ecc70158b1bfb705c722
loganmiller001 <loganmiller001@live.com>
2018-07-30 09:56:42 -0500

    Fixed merge conflicts with revert.

--- TrashCollector/Controllers/AccountController.cs
+++ TrashCollector/Controllers/AccountController.cs
@@ -486,18 +486,6 @@ namespace TrashCollector.Controllers
                 }
                 context.HttpContext.GetOwinContext().Authentication.Challenge(properties, LoginProvider);
             }
-
-            private void RedirectLogin(string username)
-            {
-                LoginRedirectByRoleSection roleRedirectSection = (LoginRedirectByRoleSection)ConfigurationManager.GetSection("loginRedirectByRole");
-                foreach (RoleRedirect roleRedirect in roleRedirectSection.RoleRedirects)
-                {
-                    if (Roles.IsUserInRole(username, roleRedirect.Role))
-                    {
-                        Response.Redirect(roleRedirect.Url);
-                    }
-                }
-            }
         }
         #endregion
     }
