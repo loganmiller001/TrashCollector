322b0da85906aa6ab8463eb55836af884184708e
loganmiller001 <loganmiller001@live.com>
2018-07-30 10:48:47 -0500

    Fixed the login screen to take just username and password, now functions correctly.

--- TrashCollector/Models/AccountViewModels.cs
+++ TrashCollector/Models/AccountViewModels.cs
@@ -48,15 +48,7 @@ namespace TrashCollector.Models
 
     public class LoginViewModel
     {
-        [Required]
-        [Display(Name = "UserRoles")]
-        public string UserRoles { get; set; }
 
-
-        [Required]
-        [Display(Name = "Email")]
-        [EmailAddress]
-        public string Email { get; set; }
         [Required]
         [Display(Name = "UserName")]
         public string UserName { get; set; }
